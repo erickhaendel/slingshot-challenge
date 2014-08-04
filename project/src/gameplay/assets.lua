305df5b7602b043da08619c91cff8d68809362cc
samuellunamartins@gmail.com <samuellunamartins@gmail.com>
2014-08-04 23:53:30 +0000

    Animcao man sprite no multiplayer gameplay

--- project/src/gameplay/assets.lua
+++ project/src/gameplay/assets.lua
@@ -224,7 +224,7 @@ function load_animation_man_sprite(color)
 		man_yellow_sprite.x =  -100 - display.contentWidth		
 		man_yellow_sprite.y = display.contentHeight - 150
 		man_yellow_sprite.isVisible = true
-		man_yellow_sprite:scale(1, 1)		
+		if man_yellow_sprite.xScale < 1 then man_yellow_sprite:scale(1.5, 1.5); end
 
 		-- transporta o man sprite para perto do estilingue
 		timer.performWithDelay( 1, function()
@@ -247,7 +247,7 @@ function load_animation_man_sprite(color)
 		man_green_sprite.isVisible = true	
 		man_green_sprite.x = display.contentWidth - 150		
 		man_green_sprite.y = display.contentHeight - 150
-		man_green_sprite:scale(1, 1)		
+		if man_green_sprite.xScale < 1 then man_green_sprite:scale(1.5, 1.5); end
 
 		-- transporta o man sprite para perto do estilingue
 		timer.performWithDelay( 1, function()
@@ -483,7 +483,7 @@ function moveCamera( )
 				man_green_sprite.x = man_green_sprite.x - configuration.camera_velocity
 			end
 			if man_yellow_sprite then
-				man_yellow_sprite.isVisible = true
+				man_yellow_sprite.isVisible = false
 				man_yellow_sprite.x = man_yellow_sprite.x - configuration.camera_velocity
 			end
 		end
@@ -496,9 +496,11 @@ function moveCamera( )
 			sky_tiles_lib.skyGroup:toBack( )
 			assetsGroup.x = assetsGroup.x + configuration.camera_velocity
 			if man_green_sprite then
+				man_green_sprite.isVisible = false				
 				man_green_sprite.x = man_green_sprite.x + configuration.camera_velocity
 			end
 			if man_yellow_sprite then
+				man_yellow_sprite.isVisible = false				
 				man_yellow_sprite.x = man_yellow_sprite.x + configuration.camera_velocity
 			end		
 		end
